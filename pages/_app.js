8196ec3b7d66d54254a8edcd4a9591f1f58fda49
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-07 12:55:37 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
 import '../styles/globals.css'
-import Link from 'next/link'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <header className="navbar">
         <div className="nav-links">
-          <Link href="/" className="nav-link">Home</Link>
-          <Link href="/about" className="nav-link">About</Link>
-          <a href="https://tour.khaliil.com/" rel="noopener noreferrer" className="nav-link">Tour</a>
-          <span id="nbold">K//</span>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="https://tour.khaliil.com/">Tour</a>
+          <a id="nbold">K//</a>
         </div>
       </header>
 
@@ -18,7 +17,7 @@ export default function App({ Component, pageProps }) {
       <footer className="footer">
         <div className="footer-links">
           <p>&copy; 2025 Khaliil</p>
-          <Link href="/legal" className="footer-link">Legal</Link>
+          <a href="/legal">Legal</a>
         </div>
       </footer>
     </>
