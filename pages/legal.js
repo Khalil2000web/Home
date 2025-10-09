af61cbc69dc8298212f013cb9700e2b7f6742421
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-09 15:46:49 +0300

    Update legal.js

--- pages/legal.js
+++ pages/legal.js
@@ -93,8 +93,23 @@ export default function Legal() {
     <>
           <Head>
         <title>LEGAL</title>
-        <meta name="description" content="Terms & Conditions and Privacy Policy of Khaliil.com" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Khaliil's Official Website" />
+        <meta name="url" content="https://www.khaliil.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Khaliil" />
+        <meta name="twitter:description" content="Khaliil's Official Website" />
+        <meta name="twitter:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:title" content="Khaliil" />
+        <meta property="og:description" content="Khaliil's Official Website" />
+        <meta property="og:image" content="/static/images/19311D40-948D-475E-A39B-9FB6AC451657.jpeg" />
+        <meta property="og:site_name" content="Khaliil" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.khaliil.com/" />
+        <meta httpEquiv="Content-type" content="text/html; charset=utf-8" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon-16x16.png" />
+        <meta name="copyright" content="2025" />
       </Head>
     <div className="content">
       <h2 id="terms-conditions">TERMS & CONDITIONS</h2>
