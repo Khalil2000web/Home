916ec95ce1353381ac35a0c9a2ae39f7559525cf
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-09 19:01:43 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import '../styles/globals.css'
 import Link from 'next/link'
+import { pagesData } from '../data/pagesData'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <header className="navbar">
+      <Head>
+        <script
+          id="__NEXT_DATA__"
+          type="application/json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(pagesData) }}
+        />
+      </Head>
+
+      <header className="navbar">
         <div className="nav-links">
           <Link className="nav-link" href="/">Home</Link>
           <Link className="nav-link" href="/about">About</Link>
-          <a className="nav-link" href="https://tour.khaliil.com/">Tour</a> {/* External links still use <a> */}
+          <a className="nav-link" href="https://tour.khaliil.com/">Tour</a>
           <img id="logo" src="/icons/logo.png" alt="Logo" />
         </div>
       </header>
@@ -24,3 +34,4 @@ export default function App({ Component, pageProps }) {
     </>
   )
 }
+
