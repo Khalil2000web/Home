089957648399aad4ab7d11e4be9a6ef474fe31fc
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-11 10:00:45 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,7 @@ import Link from 'next/link';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <header className="navbar">
-        <div className="nav-links">
-          <Link className="nav-link" href="/">Home</Link>
-          <Link className="nav-link" href="/about">About</Link>
-          <a className="nav-link" href="https://tour.khaliil.com/">Tour</a>
-          <img id="logo" src="/icons/logo.png" alt="Logo" />
-        </div>
-      </header>
+
 
       <Component {...pageProps} />
 
