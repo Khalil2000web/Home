d3b437a52d4c6ef0b6063e103194dd92b70b7dad
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-11 17:36:59 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,12 @@ import MenuOverlay from '../components/MenuOverlay';
 import Link from 'next/link';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
+return (
+<>
 
 <MenuOverlay />
-    
-      <Component {...pageProps} />
+<Component {...pageProps} />
 
-      <footer className="footer">
-        <div className="footer-links">
-          <p>&copy; 2025 Khaliil</p>
-          <Link href="/legal">Legal</Link>
-        </div>
-      </footer>
-    </>
-  );
+</>
+);
 }
