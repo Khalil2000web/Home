4138b545840911d4c3d6fd3856207114c5b4c209
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-11 16:20:26 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,7 @@ export default function App({ Component, pageProps }) {
     <>
 
 <header className="dhx">
-<button className="menu-btn" type="button" aria-label="Menu" title="Click to open menu">
-<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>
-</button>
+<MenuOverlay />
 
 <Link className="header-link" href="/">KHALIIL</Link>
 </header>
