775e66e1a7b0d41927afa759f98ea5ca87c9649e
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-11 14:37:18 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,13 @@ export default function App({ Component, pageProps }) {
   return (
     <>
 
- <header className="navbar">
-        <div className="nav-links">
-          <Link className="nav-link" href="/">Home</Link>
-          <Link className="nav-link" href="/about">About</Link>
-          <a className="nav-link" href="https://tour.khaliil.com/">Tour</a>
-          <img id="logo" src="/icons/logo.png" alt="Logo" />
-        </div>
-      </header>
+<header className="dhx">
+<button className="menu-btn" type="button" aria-label="Menu" title="Click to open menu">
+<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>
+</button>
+
+<Link className="header-link" href="/">KHALIIL</Link>
+</header>
     
       <Component {...pageProps} />
 
