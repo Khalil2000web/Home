10747cf52193eede16d497f3123ddfb653f526b8
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-20 22:16:51 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import MenuOverlay from '../components/MenuOverlay';
 import Link from 'next/link';
+import AnimatedBackground from "../components/AnimatedBackground";
 
 export default function App({ Component, pageProps }) {
 return (
 <>
 
+<AnimatedBackground />
 <MenuOverlay />
 <Component {...pageProps} />
 
