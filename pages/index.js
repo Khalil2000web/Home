75402347de07ee3283296164df097f91416b9eda
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-10-31 14:35:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -58,14 +58,22 @@ export default function HomePage() {
         
         
 <div className="homepage_gallery">
-      <img src="" alt="Home-Page Image" decoding="async" loading="eager" className="image" />
-      <img src="" alt="Home-Page Image" decoding="async" loading="eager" className="image" />
-      <img src="" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
-      <img src="" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
-      <img src="" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
-      <img src="" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
-      <img src="" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
-      <img src="" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2879.jpeg" alt="Home-Page Image" decoding="async" loading="eager" className="image" />
+      <CustomVideo
+  src="######"
+  muteIcon="/icons/icon-mute.svg"
+  unmuteIcon="/icons/icon-volume.svg"
+/>
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2908.jpeg" alt="Home-Page Image" decoding="async" loading="eager" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2909.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2915.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2934.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2973.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2938.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2944.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2945.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2946.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
+      <img src="https://pub-366eeae0aa1144bca9919dfa09bb15f7.r2.dev/IMG_2953.jpeg" alt="Home-Page Image" decoding="async" loading="lazy" className="image" />
 <CustomVideo
   src="https://ik.imagekit.io/cx6yav04a/91AAD0-2855-41E9-AFBC-BF7F0B030D1B-2.mp4"
   muteIcon="/icons/icon-mute.svg"
