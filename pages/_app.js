e8359c2bfcee3eb51352bb6605f6475921cfba82
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-11-01 11:05:55 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import '../styles/globals.css';
 import MenuOverlay from '../components/MenuOverlay';
 import Link from 'next/link';
+import localFont from 'next/font/local';
+
+const myFont = localFont({
+  src: '../public/fonts/BBManualMonoProTX.22d272fc.ttf', // Pages Router can use public folder
+  variable: '--font-main',
+});
 
 export default function App({ Component, pageProps }) {
-return (
-<>
-
-<MenuOverlay />
-<Component {...pageProps} />
-
-</>
-);
+  return (
+    <div className={myFont.variable}>
+      <MenuOverlay />
+      <Component {...pageProps} />
+    </div>
+  );
 }
