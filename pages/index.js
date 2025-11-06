43950151622830503811a53067602c719c3c617d
Khalil <165795752+Khalil2000web@users.noreply.github.com>
2025-11-06 12:45:20 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -70,6 +70,7 @@ export default function HomePage({ media }) {
         .underwork-container {
           height: 100vh;
           display: flex;
+          width:90%;
           align-items: center;
           justify-content: center;
           text-align: center;text-transform:uppercase;
